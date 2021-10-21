4181f8a4586c29ff00daaffdacb83f6b6541c356
Jacob Christian <jbc160@txstate.edu>
2021-10-20 21:25:09 -0500

    Initial commit

---
+++ type.h
@@ -0,0 +1,16 @@
+/*
+Created by: Jacob Christian
+*/
+
+#ifndef TYPE_
+#define TYPE_
+class Type {
+    public:
+        enum class EventType {ARRIVAL, DEPARTURE};
+};
+#endif //TYPE_
+
+/* Note: In order to use, you use it with as a type Type::EventType 
+   Example: Type::EventType state = Type::EventType::ARRIVAL;
+*/
+
