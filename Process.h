408788c4bd365d04f3036876aa5a104725c9ca57
Levi <levi.jacoby1991@gmail.com>
2021-10-26 17:49:45 -0500

    Uploading Process.h & Process.cpp

---
+++ Process.h
@@ -0,0 +1,26 @@
+#ifndef PROCESS
+#define PROCESS
+
+class Process{
+  public:
+    int id;
+    float arrivalTime;
+    float serviceTime;
+    float remainingServiceTime;
+    int priority;
+
+    Process(int, float, float, int);
+    //Getters
+    int getID();
+    float getArrivalTime();
+    float getServiceTime();
+    float getRemainingServiceTime();
+    int getPriority();
+    //Setters
+    void setRemainingServiceTime(float);
+    /* Sets the arrival time of the process*/
+    void setArrivalTime(float);
+    void setPriority(int);
+};
+
+#endif
