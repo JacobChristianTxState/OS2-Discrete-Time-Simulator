7ab6fdb9c74de68e2721d507c75c469a0b550015
Jacob Christian <jbc160@txstate.edu>
2021-11-09 18:28:06 -0600

    pushing to test levi's code

--- Event.cpp
+++ Event.cpp
@@ -1,9 +1,6 @@
 #include "Event.h"
 
-Event::Event(unsigned long t, int ty){
-  time = t;
-  eType = ty;
-}
+Event::Event(unsigned long t, int ty, Process* p) : time(t), eType(ty), process(p) {}
 
 int Event::getType(){
   return eType;
@@ -11,6 +8,14 @@ int Event::getType(){
 
 unsigned long Event::getTime(){
   return time;
+}
+
+void Event::setProcess(Process* p) {
+  this->process = p;
+}
+
+Process* Event::getProcess() {
+  return this->process;
 }
 
 
--- Event.h
+++ Event.h
@@ -1,14 +1,19 @@
 #ifndef EVENT_H
 #define EVENT_H
 
+#include "Process.h"
+
 class Event{
   private:
     unsigned long time;
     int eType;
+    Process* process;
   public:
-    Event(unsigned long, int);
+    Event(unsigned long, int, Process* p);
     int getType();
     unsigned long getTime();
+    void setProcess(Process* p);
+    Process* getProcess();
 };
 
 #endif
